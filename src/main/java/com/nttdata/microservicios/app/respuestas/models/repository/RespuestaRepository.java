65451911fbcb58c976cac15421c500f77a9fd713
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-04 16:29:27 +0100

    corregir error en consulta

--- src/main/java/com/nttdata/microservicios/app/respuestas/models/repository/RespuestaRepository.java
+++ src/main/java/com/nttdata/microservicios/app/respuestas/models/repository/RespuestaRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import com.nttdata.microservicios.app.respuestas.models.entity.Respuesta;
 
 public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
-	@Query("select r from Respuesta r join fetch r.pregunta p join fetch p.examen e where r.alumno_id=?1 and e.id=?2")
+	@Query("select r from Respuesta r join fetch r.pregunta p join fetch p.examen e where r.alumnoId=?1 and e.id=?2")
 	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId);
 		
-	@Query("select e.id from Respuesta r join r.pregunta p join p.examen e where r.alumno_id=?1 group by e.id ")
+	@Query("select e.id from Respuesta r join r.pregunta p join p.examen e where r.alumnoId=?1 group by e.id ")
 	public Iterable<Long> findExamenesIdsConRespuestasByAlumnoId(Long alumnoId);
 }
