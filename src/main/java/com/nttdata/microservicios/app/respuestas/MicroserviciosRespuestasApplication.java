93990f0f6aebd3c94b47f49a50a21bb64087fa2b
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-05 14:36:55 +0100

    migrar de mariaDB a MongoDB

--- src/main/java/com/nttdata/microservicios/app/respuestas/MicroserviciosRespuestasApplication.java
+++ src/main/java/com/nttdata/microservicios/app/respuestas/MicroserviciosRespuestasApplication.java
@@ -2,11 +2,8 @@ package com.nttdata.microservicios.app.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({"com.nttdata.microservicios.app.respuestas.models.entity",
-	"com.nttdata.microservicios.commons.examenes.models.entity"})
 public class MicroserviciosRespuestasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/nttdata/microservicios/app/respuestas/models/entity/Respuesta.java
+++ src/main/java/com/nttdata/microservicios/app/respuestas/models/entity/Respuesta.java
@@ -1,42 +1,36 @@
 package com.nttdata.microservicios.app.respuestas.models.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import com.nttdata.microservicios.commons.alumnos.models.entity.Alumno;
 import com.nttdata.microservicios.commons.examenes.models.entity.Pregunta;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
-@Entity
-@Table(name="respuestas")
+@Document(collection="respuestas")
 public class Respuesta {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private String id;
 	
 	private String texto;
 	
 	@Transient
 	private Alumno alumno;
 	
-	@Column(name="alumno_id")
+
 	private Long alumnoId;
 	
-	@OneToOne(fetch = FetchType.LAZY)
+	@Transient
 	private Pregunta pregunta;
+	
+	private Long preguntaId;
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
@@ -70,6 +64,14 @@ public class Respuesta {
 
 	public void setAlumnoId(Long alumnoId) {
 		this.alumnoId = alumnoId;
+	}
+
+	public Long getPreguntaId() {
+		return preguntaId;
+	}
+
+	public void setPreguntaId(Long preguntaId) {
+		this.preguntaId = preguntaId;
 	}
 	
 	
