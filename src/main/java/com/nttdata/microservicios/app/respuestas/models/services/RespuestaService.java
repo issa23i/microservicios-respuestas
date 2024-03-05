a6359ca7ab18643679bad73dda4f51d7132739f3
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-03-05 18:00:50 +0100

    feign con curso y alumno, consulta mongodb

--- src/main/java/com/nttdata/microservicios/app/respuestas/models/services/RespuestaService.java
+++ src/main/java/com/nttdata/microservicios/app/respuestas/models/services/RespuestaService.java
@@ -3,7 +3,7 @@ package com.nttdata.microservicios.app.respuestas.models.services;
 import com.nttdata.microservicios.app.respuestas.models.entity.Respuesta;
 import com.nttdata.microservicios.commons.services.CommonService;
 
-public interface RespuestaService extends CommonService<Respuesta>{
+public interface RespuestaService {
 	
 	public Iterable<Respuesta> saveAll(Iterable<Respuesta> respuestas);
 	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId);
--- src/main/java/com/nttdata/microservicios/app/respuestas/models/services/RespuestaServiceImpl.java
+++ src/main/java/com/nttdata/microservicios/app/respuestas/models/services/RespuestaServiceImpl.java
@@ -1,37 +1,52 @@
 package com.nttdata.microservicios.app.respuestas.models.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nttdata.microservicios.app.respuestas.clients.ExamenFeignClient;
 import com.nttdata.microservicios.app.respuestas.models.entity.Respuesta;
 import com.nttdata.microservicios.app.respuestas.models.repository.RespuestaRepository;
-import com.nttdata.microservicios.commons.services.CommonServiceImpl;
+import com.nttdata.microservicios.commons.examenes.models.entity.Examen;
+import com.nttdata.microservicios.commons.examenes.models.entity.Pregunta;
 
-import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-public class RespuestaServiceImpl extends CommonServiceImpl<Respuesta,RespuestaRepository> implements RespuestaService {
+public class RespuestaServiceImpl  implements RespuestaService {
 	
 	@Autowired
 	private RespuestaRepository respuestaRepository;
+	
+	private ExamenFeignClient examenClient;
 
 	@Override
-	@Transactional
 	public Iterable<Respuesta> saveAll(Iterable<Respuesta> respuestas) {
 		return respuestaRepository.saveAll(respuestas);
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId) {
-		return respuestaRepository.findRespuestaByAlumnoByExamen(alumnoId, examenId);
+		Examen examen = examenClient.obtenerExamenPorId(examenId);
+		List<Pregunta> preguntas = examen.getPreguntas();
+		List<Long> preguntaIds = preguntas.stream().map(p -> p.getId()).collect(Collectors.toList());
+		List<Respuesta> respuestas = (List<Respuesta>) respuestaRepository.findRespuestaByAlumnoByPreguntaIds(alumnoId, preguntaIds);
+		respuestas = respuestas.stream().map(r -> {
+			preguntas.forEach(p -> {
+				if(p.getId() == r.getPreguntaId()) {
+					r.setPregunta(p);
+				}
+			});
+			return r;
+		}).collect(Collectors.toList());
+		return respuestas;
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public Iterable<Long> findExamenesIdsConRespuestasByAlumnoId(Long alumnoId) {
-		return respuestaRepository.findExamenesIdsConRespuestasByAlumnoId(alumnoId);
+		return null;
 	}
 
 }
